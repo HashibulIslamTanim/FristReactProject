5a0ab41781a4aaed8c6e02dea77925ffb75fb298
Tanim <tanim@admin.com>
2025-06-09 20:22:16 +0600

    key added

--- src/Components/pages/Food.jsx
+++ src/Components/pages/Food.jsx
@@ -8,6 +8,7 @@ function Food() {
     <div className="flex flex-wrap bg-blue-950  justify-center items-center ">
       {
         loadingdata.map(product => 
+          <li key={product.id}>
         <div className="m-2 border-blue-500 rounded border-2 flex flex-col justify-center items-center bg-emerald-300 hover:shadow-2xl hover:shadow-fuchsia-500 ">
           <div className=" w-28  sm:w-2 md:w-56 h-40 md:h-60 sm:h-6 bg-cover ">
             <img className="w-full h-full" src={product.image} />
@@ -19,7 +20,7 @@ function Food() {
              <button className="bg-cyan-400 px-4  hover:cursor-pointer rounded  text-red-500">Buye Now</button>
             </div>
           
-        </div>)
+        </div> </li>)
       }
       
      
--- src/Components/pages/IslamicBook.jsx
+++ src/Components/pages/IslamicBook.jsx
@@ -9,8 +9,10 @@ function IslamicBook() {
    
     <div className="flex flex-wrap justify-center items-center bg-blue-950 ">
       {
-        loadingdata.map(product => 
+        loadingdata.map(product =>
+           <li key={product.id}>
         <div className="m-2 border-blue-500 rounded border-2 flex flex-col justify-center items-center bg-emerald-300 hover:shadow-2xl hover:shadow-fuchsia-500 ">
+         
           <div className=" w-28  sm:w-2 md:w-56 h-40 md:h-60 sm:h-6 bg-cover ">
             <img className="w-full h-full" src={product.image} />
             
@@ -20,8 +22,12 @@ function IslamicBook() {
              <p>Price: {product.price}TK</p>
              <button className="bg-cyan-400 px-4  hover:cursor-pointer rounded  text-red-500">Buye Now</button>
             </div>
-          
-        </div>)
+             
+           
+            
+        </div>
+        </li>
+         )
       }
       
      
