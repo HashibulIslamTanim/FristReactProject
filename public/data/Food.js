ca51557b596e4338d747f8354d03152a4dc9d7ed
Tanim <tanim@admin.com>
2025-06-15 18:42:01 +0600

    imgbb add

--- public/data/Food.js
+++ public/data/Food.js
@@ -5,70 +5,70 @@ const FoodData = [
         "name": "ডিম সেন্ডউইস",
         "price":"130",
         "writer":"",
-        "image":"/public/FoodAPI/Foodimage/food100.jpeg" 
+        "image":"https://i.ibb.co/FbJkhgSs/food100.jpg" 
     },
     {
         "id": "2",
         "name": "বারগার",
         "price":"100",
         "writer":"",
-        "image":"/public/FoodAPI/Foodimage/foo.jpeg" 
+        "image":"https://i.ibb.co/fzR0fgmg/foo.jpg" 
     },
     {
         "id": "3",
         "name": "চাইনিস",
         "price":"150",
         "writer":"",
-        "image":"/public/FoodAPI/Foodimage/food101 - Copy.jpeg" 
+        "image":"https://i.ibb.co/YBV31bRk/food101-Copy.jpg"
     },
     {
         "id": "4",
         "name": "নুডলুস",
         "price":"40",
         "writer":"",
-        "image":"/public/FoodAPI/Foodimage/food102.jpeg" 
+        "image":"https://i.ibb.co/4wqnRTsS/food102.jpg" 
     },
     {
         "id": "5",
         "name": "পিজ্জা",
         "price":"180",
         "writer":"",
-        "image":"/public/FoodAPI/Foodimage/food106.jpeg" 
+        "image":"https://i.ibb.co/cS0s8yw0/food106.jpg" 
     },
     {
         "id": "6",
         "name": "পাস্তা",
         "price":"60",
         "writer":"",
-        "image":"/public/FoodAPI/Foodimage/food111.jpeg" 
+        "image":"https://i.ibb.co/qFFFbFSZ/food111.jpg" 
     },
     {
         "id": "7",
         "name": "বট ঝোল",
         "price":"100",
         "writer":"",
-        "image":"/public/FoodAPI/Foodimage/food112.jpeg" 
+        "image":"https://i.ibb.co/GBhtSkB/food112.jpg" 
     },
     {
         "id": "8",
         "name": "পাকরা",
         "price":"30",
         "writer":"",
-        "image":"/public/FoodAPI/Foodimage/food116.jpeg" 
+        "image":"https://i.ibb.co/GvbtB5k2/food116.jpg" 
     },
     {
         "id": "9",
         "name": "চিকেন মাসালা",
         "price":"200",
         "writer":"",
-        "image":"/public/FoodAPI/Foodimage/food117.jpeg" 
+        "image":"https://i.ibb.co/1GGyxMDq/food117.jpg" 
     },
     {
         "id": "10",
         "name": "মোমো",
         "price":"80",
         "writer":"",
-        "image":"/public/FoodAPI/Foodimage/food118.jpeg" 
+        "image":"https://i.ibb.co/tM1yRz7Y/food118.jpg" 
     }
 ]
 export default FoodData
--- public/data/Fruit.js
+++ public/data/Fruit.js
@@ -4,70 +4,70 @@ const FruitData = [
         "name": "আপেল",
         "price":"300",
         "writer":"",
-        "image":"/public/FruitAPI/Fruitimage/fruit100.jpeg" 
+        "image":"https://i.ibb.co/5gwPtP8V/fruit100.jpg" 
     },
     {
         "id": "2",
         "name": "মালটা",
         "price":"280",
         "writer":"",
-        "image":"/public/FruitAPI/Fruitimage/fruit101.jpeg" 
+        "image":"https://i.ibb.co/vCn3KTSJ/fruit101.jpg" 
     },
     {
         "id": "3",
         "name": "পেপে",
         "price":"130",
         "writer":"",
-        "image":"/public/FruitAPI/Fruitimage/fruit102.jpeg" 
+        "image":"https://i.ibb.co/cKZXgnhM/fruit102.jpg" 
     },
     {
         "id": "4",
         "name": "স্টোবেরী",
         "price":"900",
         "writer":"",
-        "image":"/public/FruitAPI/Fruitimage/fruit103.jpeg" 
+        "image":"https://i.ibb.co/KjBq90ZN/fruit103.jpg" 
     },
     {
         "id": "5",
         "name": "তরমুজ",
         "price":"40",
         "writer":"",
-        "image":"/public/FruitAPI/Fruitimage/fruit104.jpeg" 
+        "image":"https://i.ibb.co/8gFjzS13/fruit104.jpg" 
     },
     {
         "id": "6",
         "name": "কাঠ বাদাম",
         "price":"120",
         "writer":"",
-        "image":"/public/FruitAPI/Fruitimage/fruit105.jpeg" 
+        "image":"https://i.ibb.co/NgSQjp4n/fruit105.jpg" 
     },
     {
         "id": "7",
         "name": "আনারস",
         "price":"60",
         "writer":"",
-        "image":"/public/FruitAPI/Fruitimage/fruit106.jpeg" 
+        "image":"https://i.ibb.co/TMw54g3L/fruit106.jpg" 
     },
     {
         "id": "8",
         "name": "আম",
         "price":"70",
         "writer":"",
-        "image":"/public/FruitAPI/Fruitimage/fruit107.jpeg" 
+        "image":"https://i.ibb.co/N2rTZ3WW/fruit107.jpg" 
     },
     {
         "id": "9",
         "name": "ডালিম",
         "price":"320",
         "writer":"",
-        "image":"/public/FruitAPI/Fruitimage/fruit108.jpeg" 
+        "image":"https://i.ibb.co/Hf1g99dT/fruit108.jpg" 
     },
     {
         "id": "10",
         "name": "সবেদা",
         "price":"190",
         "writer":"",
-        "image":"/public/FruitAPI/Fruitimage/fruit109.jpeg" 
+        "image":"https://i.ibb.co/gbSpgbHc/fruit109.jpg" 
     }
 ]
 export default FruitData
